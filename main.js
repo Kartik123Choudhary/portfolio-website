9758c9009cdf822a3a954108e9e1e9d09827dd67
Kartik Choudhary <133768056+Kartik123Choudhary@users.noreply.github.com>
2024-08-14 00:08:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// PORTFOLIO JAVASCRIPT CODE : 
+
+var typed = new Typed(".text",{
+    strings: ["Frontend Developer","Backend Developer","Software Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
